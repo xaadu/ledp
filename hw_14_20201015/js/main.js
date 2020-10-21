307690044dceb8b508cd171baa06f77da33224bb
Abdullah Zayed <41540861+xaadu@users.noreply.github.com>
2020-10-21 19:48:47 +0600

    Teams Section Compleated & Other Improvements
    Other Imporvements:
    - Smooth Scroll
    - Polished CSS

--- hw_14_20201015/js/main.js
+++ hw_14_20201015/js/main.js
@@ -1,10 +1,10 @@
 // Scroll Nav Effect
-$(window).on("scroll", function() {
-    if($(window).scrollTop()) {
-          $('nav').addClass('bg-black');
-    }
+$(window).on("scroll", function () {
+      if ($(window).scrollTop()) {
+            $('nav').addClass('bg-black');
+      }
 
-    else {
-          $('nav').removeClass('bg-black');
-    }
+      else {
+            $('nav').removeClass('bg-black');
+      }
 });
