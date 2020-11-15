9c0900f97255fde050d55dbd6f769a8b0bcb3b56
Abdullah Zayed <41540861+xaadu@users.noreply.github.com>
2020-11-15 22:44:41 +0600

    Events Section Completed

---
+++ hw_05_20200923/js/main.js
@@ -0,0 +1,16 @@
+$('.slider_left').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    infinite: true,
+    //autoplay: true,
+    dots: true,
+    autoplaySpeed: 3000,
+    asNavFor: '.slider_right'
+});
+$('.slider_right').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: '.slider_left',
+    arrows: false
+});
