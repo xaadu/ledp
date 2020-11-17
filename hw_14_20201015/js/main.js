fd8f065e8fabf751f11e2e76191d2e32da57c149
Abdullah Zayed <41540861+xaadu@users.noreply.github.com>
2020-11-17 06:30:43 +0600

    Upgraded Slider

--- hw_14_20201015/js/main.js
+++ hw_14_20201015/js/main.js
@@ -8,3 +8,23 @@ $(window).on("scroll", function () {
             $('nav').removeClass('bg-black');
       }
 });
+
+$('.slider_top').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      infinite: true,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      asNavFor: '.slider_bottom'
+});
+$('.slider_bottom').slick({
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      asNavFor: '.slider_top',
+      arrows: true,
+      prevArrow:'<button class="slick-prev"> &#10092; </button>',
+      nextArrow:'<button class="slick-next"> &#10093; </button>',
+      centerMode: true,
+      variableWidth: true,
+});
