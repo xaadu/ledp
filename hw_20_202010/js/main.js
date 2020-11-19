e185080ac1ac55cdb3c50d973d726634e41077f9
Abdullah Zayed <41540861+xaadu@users.noreply.github.com>
2020-11-19 20:36:47 +0600

    Structure Ready For Portfolio Template

---
+++ hw_20_202010/js/main.js
@@ -0,0 +1,9 @@
+$('.slider').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      dots: true,
+      infinite: true,
+      autoplay: true,
+      autoplaySpeed: 3000
+});
